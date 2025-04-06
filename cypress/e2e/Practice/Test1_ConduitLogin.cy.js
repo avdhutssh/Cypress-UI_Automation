1c9677faaee0c7a914ca511baa45e74edfe45549
avdhutssh <avdhut.ssh@gmail.com>
2025-04-06 13:07:10 +0530

    first Test

--- cypress/e2e/Practice/Test1_ConduitLogin.cy.js
+++ cypress/e2e/Practice/Test1_ConduitLogin.cy.js
@@ -1,7 +1,10 @@
 /// <reference types="cypress" />
 
 describe('Login Functionality',function(){
- 
+    Cypress.on('uncaught:exception', (err, runnable) => {
+        return false; // prevent app errors from failing test
+      });
+      
     it('Login Test using Conduit',function(){
         cy.visit('https://react-redux.realworld.io/')
         cy.get('a[href="#login"]').click()
