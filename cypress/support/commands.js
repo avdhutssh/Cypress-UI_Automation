17b96a24ae032f61ffae9af6cca89bb912121dc0
avdhutssh <avdhut.ssh@gmail.com>
2025-04-06 20:48:14 +0530

    added custom method for verifyMessage

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,6 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('verifyMessage', (element, expectedMessage) => {
+  element().should('be.visible').and('have.text', expectedMessage);
+});
