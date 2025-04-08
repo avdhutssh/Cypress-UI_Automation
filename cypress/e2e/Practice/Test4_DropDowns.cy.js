38c1ce49a3deca3dd37fd3740ef8c14e824550bf
avdhutssh <avdhut.ssh@gmail.com>
2025-04-08 15:50:06 +0530

    Practice -> Dynamic DropDown

--- cypress/e2e/Practice/Test4_DropDowns.cy.js
+++ cypress/e2e/Practice/Test4_DropDowns.cy.js
@@ -6,4 +6,10 @@ describe('Dropdown Functionality',function(){
         cy.get('#mySelect').select('75%').should('have.value','75%');
         cy.get('#mySelect').select(0).should('have.value','25%');
     });
+
+    it('Handling Dynamic DropDown',function(){
+        cy.visit('https://demos.telerik.com/aspnet-ajax/dropdownlist/examples/overview/defaultcs.aspx')
+        cy.get('#ctl00_ContentPlaceholder1_RadDropDownProducts').click()
+        cy.get('.rddlItem').contains('Tea').click()
+    })
 });
