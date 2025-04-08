e09d45af94b85604dc127272b34dec347ea334a0
avdhutssh <avdhut.ssh@gmail.com>
2025-04-08 15:52:36 +0530

    Practice -> Multiple DropDown

--- cypress/e2e/Practice/Test4_DropDowns.cy.js
+++ cypress/e2e/Practice/Test4_DropDowns.cy.js
@@ -11,5 +11,12 @@ describe('Dropdown Functionality',function(){
         cy.visit('https://demos.telerik.com/aspnet-ajax/dropdownlist/examples/overview/defaultcs.aspx')
         cy.get('#ctl00_ContentPlaceholder1_RadDropDownProducts').click()
         cy.get('.rddlItem').contains('Tea').click()
+    });
+
+    it('Handling Multiple DropDown',function(){
+        cy.visit('https://demo.mobiscroll.com/select/multiple-select')
+        cy.get('#multiple-select-input').click({force: true})
+        cy.get('div[class="mbsc-scroller-wheel-item mbsc-ios mbsc-ltr mbsc-wheel-item-checkmark mbsc-wheel-item-multi"]').contains('Electronics & Computers').click()
+        cy.get('div[class="mbsc-scroller-wheel-item mbsc-ios mbsc-ltr mbsc-wheel-item-checkmark mbsc-wheel-item-multi"]').contains('Health & Beauty').click()
     })
 });
