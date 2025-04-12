7bae9e1bf8b6ca44e07e449b51ee6db2d757c9bf
avdhutssh <avdhut.ssh@gmail.com>
2025-04-12 17:58:28 +0530

    Practice -> Confirmation alert - Ok button

--- cypress/e2e/Practice/Test5_Alerts.cy.js
+++ cypress/e2e/Practice/Test5_Alerts.cy.js
@@ -1,11 +1,20 @@
-describe('Advanced UI Elements',function(){
- 
-    it('Simple alert',function(){
+describe('Advanced UI Elements', function () {
+
+    it('Simple alert', function () {
         cy.visit("");
         cy.contains('Click for JS Alert').click();
         cy.on('window:alert', (alertText) => {
             expect(alertText).to.equal('I am a JS Alert');
         })
-        cy.get('#result').should('contain','You successfully clicked an alert')
+        cy.get('#result').should('contain', 'You successfully clicked an alert');
+    })
+
+    it('Confirmation alert - Ok button', function () {
+        cy.visit("");
+        cy.contains('Click for JS Alert').click();
+        cy.on('window:alert', (alertText) => {
+            (alertText).to.equal('I am a JS Confirm');
+        })
+        cy.get('#result').should('contain', 'You clicked: Ok')
     })
 })
